0248b9fac50a4b12691d589bb569995a78745e87
Jovani Segura <giovser7@gmail.com>
2024-03-19 20:18:56 -0500

    Agregado el fondo del index y el mostrado de los logos mediante el input

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ let equipoIzq = document.getElementById("equipoIzq");
 let equipoDer = document.getElementById("equipoDer");
 let boton = document.querySelector(".boton");
 let infoRegNombre = document.querySelectorAll(".infoRegNombre");
+let infoEscEquipo = document.querySelectorAll(".infoEscEquipo");
+let equiRealIzq = document.querySelector(".equiRealIzq");
+let equiBarcaIzq = document.querySelector(".equiBarcaIzq");
+let equiRealDer = document.querySelector(".equiRealDer");
+let equiBarcaDer = document.querySelector(".equiBarcaDer");
 
 function validarCampos() {
   if (
@@ -22,15 +27,15 @@ nombreIzq.addEventListener("input", () => {
   if (nombreIzq.value.match(/[a-zA-ZñÑ]/)) {
     if (nombreIzq.value.toLowerCase() === nombreDer.value.toLowerCase()) {
       infoRegNombre[0].textContent = "Por favor escoge un nombre diferente";
-      infoRegNombre[0].style.color = 'red'
+      infoRegNombre[0].style.color = "red";
       nombreIzq.value = "";
     } else {
       infoRegNombre[0].textContent = "Nombre OK";
-      infoRegNombre[0].style.color = 'green'
+      infoRegNombre[0].style.color = "green";
     }
   } else {
     infoRegNombre[0].textContent = "Nombre Incorrecto";
-    infoRegNombre[0].style.color = 'red'
+    infoRegNombre[0].style.color = "red";
   }
   validarCampos();
 });
@@ -39,36 +44,57 @@ nombreDer.addEventListener("input", () => {
   if (nombreDer.value.match(/[a-zA-ZñÑ]/)) {
     if (nombreDer.value.toLowerCase() === nombreIzq.value.toLowerCase()) {
       infoRegNombre[1].textContent = "Por favor escoge un nombre diferente";
-      infoRegNombre[1].style.color = 'red'
+      infoRegNombre[1].style.color = "red";
       nombreDer.value = "";
     } else {
       infoRegNombre[1].textContent = "Nombre OK";
-      infoRegNombre[1].style.color = 'green'
+      infoRegNombre[1].style.color = "green";
     }
   } else {
     infoRegNombre[1].textContent = "Nombre Incorrecto";
-    infoRegNombre[1].style.color = 'red'
+    infoRegNombre[1].style.color = "red";
   }
   validarCampos();
 });
 
-
 equipoIzq.addEventListener("change", () => {
   if (equipoIzq.value === equipoDer.value) {
-    console.log("Los equipos no pueden ser los mismos");
+    infoEscEquipo[0].textContent = "Los equipos no pueden ser los mismos";
+    infoEscEquipo[0].style.color = "red";
     equipoIzq.value = "";
+    equiRealIzq.style.display = "none";
+    equiBarcaIzq.style.display = "none";
   } else {
-    console.log("El equipo escogido por el Jugador Local es: " + equipoIzq.value);
+    infoEscEquipo[0].textContent = "Equipo OK";
+    infoEscEquipo[0].style.color = "green";
+    if (equipoIzq.value === "real_madrid") {
+      equiRealIzq.style.display = "inline";
+      equiBarcaIzq.style.display = "none";
+    } else {
+      equiBarcaIzq.style.display = "inline";
+      equiRealIzq.style.display = "none";
+    }
   }
   validarCampos();
 });
 
 equipoDer.addEventListener("change", () => {
   if (equipoDer.value === equipoIzq.value) {
-    console.log("Los equipos no pueden ser los mismos");
+    infoEscEquipo[1].textContent = "Los equipos no pueden ser los mismos";
+    infoEscEquipo[1].style.color = "red";
     equipoDer.value = "";
+    equiRealDer.style.display = "none";
+    equiBarcaDer.style.display = "none";
   } else {
-    console.log("El equipo escogido por el Jugador Visitante es: " + equipoDer.value);
+    infoEscEquipo[1].textContent = "Equipo OK";
+    infoEscEquipo[1].style.color = "green";
+    if (equipoDer.value === "real_madrid") {
+      equiRealDer.style.display = "inline";
+      equiBarcaDer.style.display = "none";
+    } else {
+      equiBarcaDer.style.display = "inline";
+      equiRealDer.style.display = "none";
+    }
   }
   validarCampos();
 });
